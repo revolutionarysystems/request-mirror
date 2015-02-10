a59e9e9a1dff38bce5d14da947d4b191a4dcd696
Adam Knight <adam@revolutionarysystems.co.uk>
2015-02-10 16:19:37 +0000

    Maintain order of headers

--- src/main/java/uk/co/revsys/requestmirror/RequestMirrorServlet.java
+++ src/main/java/uk/co/revsys/requestmirror/RequestMirrorServlet.java
@@ -3,11 +3,13 @@ package uk.co.revsys.requestmirror;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
 import uk.co.revsys.ripe.client.RipeClient;
 import uk.co.revsys.ripe.client.RipeSearchResult;
 
@@ -23,27 +25,31 @@ public class RequestMirrorServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        JSONObject json = new JSONObject();
         String ipAddress = req.getRemoteAddr();
-        json.put("ipAddress", ipAddress);
-        json.put("timestamp", new Date().getTime());
-        JSONObject headersJson = new JSONObject();
+        if(req.getParameter("RevSysOverrideIPAddress")!=null){
+            ipAddress = req.getParameter("RevSysOverrideIPAddress");
+        }
+        HashMap data = new HashMap();
+        data.put("ipAddress", ipAddress);
+        data.put("timestamp", new Date().getTime());
         Enumeration<String> headerNames = req.getHeaderNames();
+        LinkedHashMap<String, String> headers = new LinkedHashMap<String, String>();
         while (headerNames.hasMoreElements()) {
             String headerName = headerNames.nextElement();
             String headerValue = req.getHeader(headerName);
-            headersJson.put(headerName, headerValue);
+            headers.put(headerName, headerValue);
         }
-        json.put("headers", headersJson);
+        
+        data.put("headers", headers);
         if ("true".equals(req.getParameter("includeNetwork"))) {
             RipeSearchResult searchResult = ripeClient.search(ipAddress);
-            JSONObject networkJSON = new JSONObject();
-            networkJSON.put("name", searchResult.getNetworkName());
-            networkJSON.put("description", searchResult.getNetworkDescription());
-            json.put("network", networkJSON);
+            HashMap network = new HashMap();
+            network.put("name", searchResult.getNetworkName());
+            network.put("description", searchResult.getNetworkDescription());
+            data.put("network", network);
         }
         resp.setContentType("application/json");
-        resp.getOutputStream().print(json.toString());
+        resp.getOutputStream().print(JSONObject.toJSONString(data));
         resp.getOutputStream().flush();
     }
 
